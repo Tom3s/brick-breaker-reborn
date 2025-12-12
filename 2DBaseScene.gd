e36e465c5ff060c8ac7fb93481f7b0b1bc5dbe90
Tom3s <55595037+Tom3s@users.noreply.github.com>
2025-12-12 13:02:51 +0200

    [#1] Added collision logic with lines and normals  (#3)
    * Line Collision Without Corners
    
    Implemented line collision, but it does not take into account the edges of the line
    
    * Added edge (hehe) cases
    
    Ball can now bounce correctly from the edge of the lines
    
    * Added line collision for screen bounds
    
    Screen border collisions are now handled through 4 separate line segment collisions, removing old collision logic

---
+++ 2DBaseScene.gd
@@ -0,0 +1,151 @@
+extends Node2D
+# class_name # not requiered for test
+
+class Ball:
+	var radius: float = 16.0
+	var position: Vector2
+	var velocity: Vector2
+	
+	var target_velocity: float = 512.0
+	
+	var deceleration: float = 8.0
+	
+	var speed_up_factor: float = 2.0
+
+
+@onready var collider_line_scene: PackedScene = preload("res://ColliderLine.tscn")
+
+@onready var ball_sprite: Sprite2D = %BallSprite
+@onready var line_parent: Node2D = %Lines
+
+var ball: Ball = Ball.new()
+
+func _ready() -> void:
+	ball.velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	ball.velocity *= ball.target_velocity
+
+	# screen_bounds = DisplayServer.window_get_size()
+	set_up_screen_collision()
+
+
+func _process(delta: float) -> void:
+	if ball.velocity.length() > ball.target_velocity:
+		ball.velocity = ball.velocity.normalized() * (ball.velocity.length() - ball.deceleration)
+	else:
+		ball.velocity = ball.velocity.normalized() * ball.target_velocity
+	
+	ball.position += ball.velocity * delta
+
+	# handle collision
+	var collided: bool = false
+
+	for collider_line in line_parent.get_children():
+		collided = collided || handle_line_collision(collider_line)
+
+	
+
+	if collided:
+		ball.velocity = ball.velocity.normalized() * ball.target_velocity * ball.speed_up_factor
+
+	ball_sprite.global_position = ball.position
+
+func set_up_screen_collision() -> void:
+	var screen_bounds: Vector2 = DisplayServer.window_get_size()
+
+	var p1: Vector2 = Vector2(-screen_bounds.x / 2, -screen_bounds.y / 2)
+	var p2: Vector2 = Vector2(screen_bounds.x / 2, -screen_bounds.y / 2)
+	var p3: Vector2 = Vector2(screen_bounds.x / 2, screen_bounds.y / 2)
+	var p4: Vector2 = Vector2(-screen_bounds.x / 2, screen_bounds.y / 2)
+
+	var line: ColliderLine = collider_line_scene.instantiate()
+	line_parent.add_child(line)
+	line.set_points(p1, p2)
+
+	line = collider_line_scene.instantiate()
+	line_parent.add_child(line)
+	line.set_points(p2, p3)
+
+	line = collider_line_scene.instantiate()
+	line_parent.add_child(line)
+	line.set_points(p3, p4)
+
+	line = collider_line_scene.instantiate()
+	line_parent.add_child(line)
+	line.set_points(p4, p1)
+
+
+
+
+# func handle_screen_collision() -> bool:
+# 	var collided: bool = false
+# 	# collision on Y
+# 	if ball.velocity.y > 0:
+# 		if ball.position.y + ball.radius > screen_bounds.y / 2:
+# 			ball.velocity.y *= -1
+# 			ball.position.y -= screen_bounds.y / 2 - (ball.position.y + ball.radius)
+# 			collided = true
+# 	else:
+# 		if ball.position.y - ball.radius < -screen_bounds.y / 2:
+# 			ball.velocity.y *= -1
+# 			ball.position.y +=  -screen_bounds.y / 2 - (ball.position.y - ball.radius)
+# 			collided = true
+	
+# 	# collision on X
+# 	if ball.velocity.x > 0:
+# 		if ball.position.x + ball.radius > screen_bounds.x / 2:
+# 			ball.velocity.x *= -1
+# 			ball.position.x -= screen_bounds.x / 2 - (ball.position.x + ball.radius)
+# 			collided = true
+# 	else:
+# 		if ball.position.x - ball.radius < -screen_bounds.x / 2:
+# 			ball.velocity.x *= -1
+# 			ball.position.x +=  -screen_bounds.x / 2 - (ball.position.x - ball.radius)
+# 			collided = true
+	
+# 	return collided
+
+func handle_line_collision(line: ColliderLine) -> bool:
+	var collided: bool = false
+
+	var p1: Vector2 = line.debug_point1.global_position
+	var p2: Vector2 = line.debug_point2.global_position
+
+	var moving_towards_line: bool = ball.velocity.dot(line.normal) < 0
+
+	line.set_moving_towards(moving_towards_line)
+
+	# idfk what im doing here, but it's dot product magic
+	# for more info see: https://youtu.be/nXrEX6j-Mws?si=8GdqyyBu0hQkDFsm&t=224
+	var distance_from_line: float = abs((ball.position - p1).dot(line.normal))
+
+	var case: float = (ball.position - p1).dot(line.tangent)
+
+	var current_normal: Vector2 = line.normal
+
+	if case < 0:
+		distance_from_line = (ball.position - p1).length()
+		current_normal = (ball.position - p1).normalized()
+	elif case > (p1 - p2).length():
+		distance_from_line = (ball.position - p2).length()
+		current_normal = (ball.position - p2).normalized()
+
+	if (distance_from_line < ball.radius):
+		var speed_along_normal: float = ball.velocity.dot(current_normal)
+
+		if speed_along_normal <= 0:
+			var correction: float = ball.radius - distance_from_line
+
+			ball.position += current_normal * correction * 2
+
+			ball.velocity *= -1
+			var angle: float = ball.velocity.angle_to(current_normal)
+			ball.velocity = ball.velocity.rotated(2 * angle)
+
+			collided = true
+		
+
+
+
+	# print(distance_from_line)
+
+	return collided
---
+++ ColliderLine.gd
@@ -0,0 +1,63 @@
+@tool
+extends Node2D
+class_name ColliderLine
+
+@onready var debug_point1: Node2D = %DebugPoint1
+@onready var debug_point2: Node2D = %DebugPoint2
+@onready var debug_line: Polygon2D = %DebugLine
+@onready var debug_normal: Polygon2D = %DebugNormal
+
+@export
+var show_debug: bool = false
+
+var p1: Vector2
+var p2: Vector2
+
+var tangent: Vector2
+var normal: Vector2
+
+func _ready() -> void:
+	p1 = debug_point1.global_position
+	p2 = debug_point2.global_position
+
+	tangent = (debug_point2.global_position - debug_point1.global_position).normalized()
+	normal = tangent.rotated(PI / 2)
+
+func _process(delta: float) -> void:
+	if show_debug:
+		tangent = (debug_point2.global_position - debug_point1.global_position).normalized()
+		normal = tangent.rotated(PI / 2)
+
+		var points: PackedVector2Array
+		
+		points.append(debug_point1.global_position + normal)
+		points.append(debug_point1.global_position - normal)
+		points.append(debug_point2.global_position - normal)
+		points.append(debug_point2.global_position + normal)
+
+		debug_line.polygon = points
+
+
+		var normal_points: PackedVector2Array
+
+		var half_point: Vector2 = lerp(debug_point1.global_position, debug_point2.global_position, 0.5)
+
+		normal_points.append(half_point.move_toward(debug_point1.global_position, 1.0))
+		normal_points.append(half_point.move_toward(debug_point2.global_position, 1.0))
+		normal_points.append(half_point + normal * 16)
+
+		debug_normal.polygon = normal_points
+
+
+func set_moving_towards(moving_towards: bool) -> void:
+	if moving_towards:
+		debug_line.color = Color.from_string("ff220067", Color.WHITE)
+	else:
+		debug_line.color = Color.from_string("0088ff67", Color.WHITE)
+	
+func set_points(p1_new: Vector2, p2_new: Vector2) -> void:
+	p1 = p1_new
+	p2 = p2_new
+
+	debug_point1.global_position = p1
+	debug_point2.global_position = p2
