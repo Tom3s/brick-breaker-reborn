d22288890ce49e3b394371a1120f688f918141f1
Tom3s <tomesjerry3@gmail.com>
2026-07-01 18:50:44 +0300

    Added ice sfx

--- audio/SFXPlayer.gd
+++ audio/SFXPlayer.gd
@@ -4,6 +4,7 @@ class_name SFXPlayer
 @onready var ball_hit: AudioStreamPlayer = %BallHit
 
 @onready var normal_block_hit: AudioStreamPlayer = %NormalBlock
+@onready var ice_block_hit: AudioStreamPlayer = %IceBlock
 
 func play_ball_hit() -> void:
 	ball_hit.play()
@@ -12,4 +13,4 @@ func play_block_hit(type: BreakableBlock.BlockType) -> void:
 	if type == BreakableBlock.BlockType.NORMAL:
 		normal_block_hit.play()
 	elif type == BreakableBlock.BlockType.ICE:
-		pass
+		ice_block_hit.play(.464)
